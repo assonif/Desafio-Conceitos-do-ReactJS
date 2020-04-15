deca51e2987b0a1dc2dfa8188c08cf34d3ae37bd
Fabio Furtado <fabioassoni06@gmail.com>
2020-04-14 22:40:20 -0300

    Desafio finalizado

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from "react";
+
+import api from './services/api';
+
+import "./styles.css";
+
+function App() {
+
+  const [ repositories, setRepositories ] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data)
+    });
+  }, [])
+
+  async function handleAddRepository() {
+    const response = await api.post('repositories', {
+      title: Date.now(),
+      url: "teste",
+      techs: "node"
+    })
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository])
+
+    console.log(repositories)
+  }
+
+  async function handleRemoveRepository(id) {
+  
+    await api.delete(`repositories/${id}`);
+
+    setRepositories(repositories.filter(repository => repository.id !== id));
+  }
+
+    
+
+  return (
+    <div>
+      <ul data-testid="repository-list">
+        {repositories[0] && repositories.map(repository => {
+          return(
+            <li key={repository.id}>
+              {repository.title}
+
+              <button onClick={() => handleRemoveRepository(repository.id)}>
+                Remover
+              </button>
+            </li>
+          )
+        })}
+        
+      </ul>
+
+      <button onClick={handleAddRepository}>Adicionar</button>
+    </div>
+  );
+}
+
+export default App;
